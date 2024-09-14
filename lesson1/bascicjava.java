4112f5385455ecde02d1ccefa1c00bdf8e0903af
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-09-14 06:43:14 +0000

    basic structure,variables,string and its method

---
+++ lesson1/bascicjava.java
@@ -0,0 +1,20 @@
+//first program in java 
+//basic syntax
+
+public class bascicjava{
+
+    public static void main(String args[]){
+
+        System.out.println("hello world");
+        System.out.print("hey");
+        int age=20;
+        long no=3000000L;
+        String name="mehak";
+        int len=name.length();
+        System.out.printf("name is %s length of name is %d ",name,len);
+        String name1="mak";
+        String name2="tak";
+        System.out.println(name1 + name2);
+        System.out.printf( "%d  %d",age,no);
+    }
+}
