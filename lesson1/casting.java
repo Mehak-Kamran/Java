80dbecab7e1f307cffa7e4927349b3acf08f5b89
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-09-16 17:23:50 +0000

    implicit,explicit and constant

---
+++ lesson1/casting.java
@@ -0,0 +1,30 @@
+//implicit casting and explicit casting
+
+public class casting{
+
+public static void main(String args[]){
+    System.out.println("Casting");
+    //implicit
+    int weight=45;
+    float weight1=weight;
+    System.out.println(weight1);
+
+    //explicit
+    float age=23.6F;
+    int age2=(int)age;
+    System.out.println(age2);
+
+    //constant
+    final float PI=3.142F;
+    System.out.println(PI);//WE USE CAPITAL LETTERS FOR DEFINING CONSTANT IN JAVA
+    // pi=2;
+    // System.out.println(pi);//thus value is changed
+    
+
+
+
+
+
+}
+}
+
