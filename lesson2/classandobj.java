1b6c54f4689aa65a82437cad1623480712302536
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-09-20 09:21:20 +0000

    basic concept about class and obj

---
+++ lesson2/classandobj.java
@@ -0,0 +1,30 @@
+//class are blueprint/structure/mold and obj are the creations
+
+//class
+//class have properties and methods
+class Student{
+    String name;
+    int age;
+
+   void study(){
+    System.out.printf("%s,%d",this.name,this.age);
+    
+   }
+}
+
+
+
+public class classandobj{
+
+
+
+
+    public static void main(String[] args){
+        //creating obj
+        Student s1=new Student();
+        s1.name="mehak";
+        s1.age=21;
+        s1.study();
+
+    }
+}
