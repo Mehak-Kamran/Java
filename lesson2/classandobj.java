a9dc0464f2ad1c05e3cadbcbbe1527190d044486
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-09-20 09:39:54 +0000

    parametrized and non parametrized construcor

--- lesson2/classandobj.java
+++ lesson2/classandobj.java
@@ -10,6 +10,18 @@ class Student{
     System.out.printf("%s,%d",this.name,this.age);
     
    }
+//constructor
+
+//nonparmetrized
+//    Student(){
+//     System.out.println("hello");
+//    }
+
+//parametrized
+Student(String name , int age){
+         this.name=name;
+         this.age=age;
+        }
 }
 
 
@@ -21,11 +33,11 @@ public class classandobj{
 
     public static void main(String[] args){
         //creating obj
-        Student s1=new Student();
+        Student s1=new Student("mak",23);
         s1.name="mehak";
         s1.age=21;
         s1.study();
-        Student s2=new Student();
+        Student s2=new Student("ayesha",22);
         s2.name="ayesha";
         s2.age=24;
         s2.study();
