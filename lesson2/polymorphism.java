523728af4a3ca28631c0f618542dbbda3af1325a
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-09-23 03:07:58 +0000

    compile time polymorphism

--- lesson2/polymorphism.java
+++ lesson2/polymorphism.java
@@ -1,14 +1,37 @@
-class polymorphism{
+//polymorphism
+//2 types--function overloading (compile time), function overriding(run time)
 
-  public class Data{
-    int number;
-    String school;
-  }
+//function overloading (compile time)
+
+class House{
+     String color;
+     int no_of_doors;
+
+     public void houseinfo(String color){
+        this.color=color;
+        System.out.println(this.color);
+
+     }
+     public void houseinfo(int no_of_doors){
+        this.no_of_doors=no_of_doors;
+        System.out.println(this.no_of_doors);
+
+     }
+}
+
+
+
+
+public class polymorphism{
 
 
 
     public static void main(String[] args){
+        //crete obj
+        House house1=new House();
+        house1.houseinfo("blue");
+    
+        
 
     }
-
 }
